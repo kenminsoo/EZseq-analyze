6c24e5f30d2220725fef29805fb6d69f3c11d5ef
Ken Nakatsu <minsookatsu@gmail.com>
2022-08-17 21:46:07 -0400

    first commit

---
+++ fastq-rawcount/README.md
@@ -0,0 +1,76 @@
+# Simple Aligner and Annotater
+## Ken Nakatsu 07/27/2022
+
+##Citations
+##Bioconda
+Grüning, Björn, Ryan Dale, Andreas Sjödin, Brad A. Chapman, Jillian Rowe, Christopher H. Tomkins-Tinch, Renan Valieris, the Bioconda Team, and Johannes Köster. 2018. Bioconda: Sustainable and Comprehensive Software Distribution for the Life Sciences. Nature Methods, 2018 doi:10.1038/s41592-018-0046-7.
+##FastQC
+Andrews, S. (2010). FastQC:  A Quality Control Tool for High Throughput Sequence Data [Online]. Available online at: http://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+## AdapterRemoval
+Lindgreen (2012): AdapterRemoval: Easy Cleaning of Next Generation Sequencing Reads, BMC Research Notes, 5:337 http://www.biomedcentral.com/1756-0500/5/337/
+## Bowtie2
+Langmead B, Salzberg S. Fast gapped-read alignment with Bowtie 2. Nature Methods. 2012, 9:357-359.
+
+##FeatureCounts
+Liao Y, Smyth GK, Shi W. featureCounts: an efficient general purpose program for assigning sequence reads to genomic features. Bioinformatics. 2014 Apr 1;30(7):923-30. doi: 10.1093/bioinformatics/btt656. Epub 2013 Nov 13. PMID: 24227677.
+## piRNA Database
+ https://www.pirnadb.org/about/informations/pirna
+## all smRNA Database
+https://dashr2.lisanwanglab.org/about.php
+##miRBase
+miRBase: from microRNA sequences to function.
+Kozomara A, Birgaoanu M, Griffiths-Jones S.
+Nucleic Acids Res 2019 47:D155-D162
+
+miRBase: annotating high confidence microRNAs using deep sequencing data.
+Kozomara A, Griffiths-Jones S.
+Nucleic Acids Res 2014 42:D68-D73
+
+miRBase: integrating microRNA annotation and deep-sequencing data.
+Kozomara A, Griffiths-Jones S.
+Nucleic Acids Res 2011 39:D152-D157
+
+miRBase: tools for microRNA genomics.
+Griffiths-Jones S, Saini HK, van Dongen S, Enright AJ.
+Nucleic Acids Res 2008 36:D154-D158
+
+miRBase: microRNA sequences, targets and gene nomenclature.
+Griffiths-Jones S, Grocock RJ, van Dongen S, Bateman A, Enright AJ.
+Nucleic Acids Res 2006 34:D140-D144
+
+The microRNA Registry.
+Griffiths-Jones S.
+Nucleic Acids Res 2004 32:D109-D111
+
+## Regular hg38 annotation
+##### https://www.ncbi.nlm.nih.gov/assembly/GCF_000001405.26/
+
+#### CircRNA **ADD**
+http://yang-laboratory.com/circpedia/download
+
+
+
+This tool outputs raw count tables. Normalize in R or a programming language of your choice. 
+
+### Usage
+
+cd into this working directory
+It should have two folders: 
+One with fastq.gz samples that have already been combined
+    Named "unaligned"
+    It should also have an index genome.
+    
+It should have another directory named 'annotation'
+    It will have four files for four different types of
+     annotation.
+     
+All files will be written to output. 
+
+Post adapter removal QC was omitted.  
+
+
+
+chmod +x align.sh
+bash align.sh
+
+done.
---
+++ fastq-rawcount/align.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+#  align.sh
+#  
+#
+#  Created by Ken Nakatsu on 7/27/22.
+#  
+
+#With eight cores, this pipeline runs at a speed of 45 seconds per sample.
+
+#this script will do:
+# 1) run a quality check
+# 2) remove adapters based upon a given adapter seq
+# 3) run a post-trim quality check
+# 4) run alignment with bowtie 2 against hg38
+# 5) run annotation with four different files
+# 5.1) all, all_smrna, mirna, pirna
+#start the script in a dir with two files, annotation and unaligned files with ref. genome
+#ensure that you have conda installed with all packages
+mkdir output
+#all output files will be stored here
+mkdir output/fastqc_analysis
+mkdir output/fastqc_analysis/unprocessed
+#fastq
+mkdir output/fastqc_analysis/docs
+#zips and logs
+mkdir output/fastqc_analysis/results
+#html files
+
+#run fastqc with 8 threads. move to respective directory
+fastqc unaligned/*.fastq.gz -t 8 -o output/fastqc_analysis
+mv output/fastqc_analysis/*.html output/fastqc_analysis/results
+mv output/fastqc_analysis/*.zip output/fastqc_analysis/docs
+
+#user can input a new adapter or add another one with --adapter2 '$user_adapter2'
+#this will remove adapters and move it to its respective dir
+
+user_adapter1="AACTGTAGGCACCATCAAT"
+user_adapter2=""
+
+cd unaligned
+
+#var%%.x will remove everything after . i.e. y.x -> y
+
+for f in *.fastq.gz ; do
+    file="$f"
+    sampname="${file%%.*}"
+    AdapterRemoval --file1 "${file}" --adapter1 AACTGTAGGCACCATCAAT --basename "trimmed"_"${sampname}" --gzip --threads 8 --trimqualities --minlength 20
+done
+
+cd ..
+
+mkdir output/adaptertrim
+mkdir output/adaptertrim/discarded_ar
+mkdir output/adaptertrim/settings
+
+mv unaligned/*.discarded.gz output/adaptertrim/discarded_ar
+mv unaligned/*.settings output/adaptertrim/settings
+mv unaligned/*.truncated.gz output/adaptertrim
+
+#change into fastq file
+#note: ${var//x/y} will take the variable, remove x from it, and replace it with y
+for f in output/adaptertrim/*.gz ; do
+    mv $f ${f//truncated/fastq};
+done
+
+#run post trimming QC
+mkdir output/fastqc_post_trim
+fastqc output/adaptertrim/*.fastq.gz -t 8 -o output/fastqc_post_trim
+
+#now lets align and move those aligned files to a new folder
+
+mkdir output/aligned
+
+for f in output/adaptertrim/*.fastq.gz; do
+    file="$f"
+    sampname="${file%%.*}"
+    bowtie2 -x unaligned/grch38_1kgmaj -U $f -S "${sampname}".sam -p 8
+done
+    
+for f in output/adaptertrim/*.sam; do
+    mv $f output/aligned
+done
+
+#run featureCounts for each one of the annotation files
+#hardcoded, sorry
+
+featureCounts -T 8 -a annotation/hsa_all.gff3 -F 'GTF' -g 'gene_name' -o "hsa_all.tsv" output/aligned/*.sam
+featureCounts -T 8 -a annotation/hsa_all_smrna.gff -F 'GTF' -t 'lnc_RNA' -g 'gene_id'  -o "hsa_all_smrna.tsv" output/aligned/*.sam
+featureCounts -T 8 -a annotation/hsa_mirna.gff3 -F 'GTF' -t 'miRNA' -g 'Name' -o "hsa_mirna.tsv" output/aligned/*.sam
+featureCounts -T 8 -a annotation/hsa_pirna.gtf -F 'GTF' -t 'piRNA' -g 'piRNA_code' -o "hsa_pirna.tsv" output/aligned/*.sam
+
+mkdir output/counts
+mv *.tsv output/counts
+for f in *.summary ; do
+    mv $f ${f//.tsv.summary/.summary.tsv};
+done
+mv *.tsv output/counts
+
+#With eight cores, this pipeline runs at a speed of 45 seconds per sample, 18 minutes for 23 samples. 
