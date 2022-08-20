30d4b0fc5680b17338297fe1e5506a7e6cdcccdd
Ken Nakatsu <minsookatsu@gmail.com>
2022-08-19 23:02:34 -0400

    removed test file

--- fastq-rawcount/align.sh
+++ fastq-rawcount/align.sh
@@ -6,7 +6,7 @@
 #  Created by Ken Nakatsu on 7/27/22.
 #  
 
-#With eight cores, this pipeline runs at a speed of 45 seconds per sample. ~4mil rd per sample
+#With eight cores, this pipeline runs at a speed of 45 seconds per sample.
 
 #this script will do:
 # 1) run a quality check
@@ -18,9 +18,10 @@
 #start the script in a dir with two files, annotation and unaligned files with ref. genome
 #ensure that you have conda installed with all packages
 eval "$(conda shell.bash hook)"
-conda activate smrnaseq_ddocent_env
+conda activate ezanalyze
 
 mv $unalign_dir unaligned
+
 
 mkdir output
 #all output files will be stored here
@@ -80,8 +81,6 @@ for f in output/adaptertrim/*.fastq.gz; do
     sampname="${file%%.*}"
     bowtie2 -x unaligned/grch38_1kgmaj -U $f -S "${sampname}".sam -p $threads
 done
-#40% alignment for media
-#90-98% for cell
 
 for f in output/adaptertrim/*.sam; do
     mv $f output/aligned
@@ -93,25 +92,21 @@ if (($reg==1))
 then
     featureCounts -T $threads -a annotation/hsa_all.gff3 -F 'GTF' -g 'gene_name' -o "hsa_all.tsv" output/aligned/*.sam
 fi
-#~30%
 
 if (($sm_all==1))
 then
     featureCounts -T $threads -a annotation/hsa_all_smrna.gff -F 'GTF' -t 'lnc_RNA' -g 'gene_id'  -o "hsa_all_smrna.tsv" output/aligned/*.sam
 fi
-#30-40%
 
 if (($mirna==1))
 then
     featureCounts -T $threads -a annotation/hsa_mirna.gff3 -F 'GTF' -t 'miRNA' -g 'Name' -o "hsa_mirna.tsv" output/aligned/*.sam
 fi
-#7-50%
 
 if (($pirna==1))
 then
     featureCounts -T $threads -a annotation/hsa_pirna.gtf -F 'GTF' -t 'piRNA' -g 'piRNA_code' -o "hsa_pirna.tsv" output/aligned/*.sam
 fi
-#1-12%
 
 mkdir output/counts
 mv *.tsv output/counts
@@ -127,4 +122,5 @@ mv output/counts/*.tsv ../rawcount-analysis/counts
 
 touch output/counts/note.txt
 echo 'raw counts are now in the rawcount-analysis/counts folder' > output/counts/note.txt
+
 #With eight cores, this pipeline runs at a speed of 45 seconds per sample, 18 minutes for 23 samples. 
